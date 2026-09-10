1997d9f96667fcd8b28bc3ba04eb979fee765263
Amateur Programmer <solo2244rt@gmail.com>
2026-09-10 14:41:56 +0700

    Implement MainScaffold with navigation and screens

---
+++ lib/widgets/main_scaffold.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import '../screens/feed_screen.dart';
+import '../screens/games_screen.dart';
+import '../screens/profile_screen.dart';
+
+class MainScaffold extends StatefulWidget {
+  const MainScaffold({super.key});
+
+  @override
+  State<MainScaffold> createState() => _MainScaffoldState();
+}
+
+class _MainScaffoldState extends State<MainScaffold> {
+  int currentIndex = 0;
+
+  final List<Widget> screens = const [
+    FeedScreen(),
+    GamesScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: currentIndex,
+        children: screens,
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: currentIndex,
+        onDestinationSelected: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.explore_outlined),
+            selectedIcon: Icon(Icons.explore),
+            label: 'Лента',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.sports_esports_outlined),
+            selectedIcon: Icon(Icons.sports_esports),
+            label: 'Игры',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person_outline),
+            selectedIcon: Icon(Icons.person),
+            label: 'Профиль',
+          ),
+        ],
+      ),
+    );
+  }
+}
